6c81442fa2d980f5785478bba30fe41a2a71c80a
aryan nama <aryan.nama001@gmail.com>
2022-12-08 23:15:14 +0530

    dfs

--- DFS.cpp
+++ DFS.cpp
@@ -2,28 +2,22 @@
 using namespace std;
 
 class Solution {
-  public:
-    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
-       int vis[V] = {0};
-       vector<int> bfs;
-
-       queue<int> q;
-       q.push(0);
-       vis[0]=1;
-       while(!q.empty()){
-        int node = q.front();
-        q.pop();
-        bfs.push_back(node);
+    void dfs(int node,vector<int> adj[],vector<int> &ans,int vis[]){
+        vis[node]=1;
+        ans.push_back(node);
 
         for(auto it:adj[node]){
-            if(!vis[it]){
-                vis[it]=1;
-                q.push(it);
-            }
+            if(!vis[it])
+                dfs(it,adj,ans,vis);
         }
-       }
-       return bfs;
+    }
+  public:
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        int vis[V]={0};
+       vector<int> ans;
+         dfs(0,adj,ans,vis);
 
+         return ans;
     }
 };
 
@@ -42,11 +36,10 @@ int main() {
             cin >> u >> v;
             adj[u].push_back(v);
             adj[v].push_back(u);
-
         }
         
         Solution obj;
-        vector<int> ans = obj.bfsOfGraph(V, adj);
+        vector<int> ans = obj.dfsOfGraph(V, adj);
         for (int i = 0; i < ans.size(); i++) {
             cout << ans[i] << " ";
         }
